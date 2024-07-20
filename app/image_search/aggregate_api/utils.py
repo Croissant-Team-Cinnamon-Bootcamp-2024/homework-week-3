6f766fb335a2551d88330de45279fad4b2e4781f
huy.ng <nguyenduchuy1711@gmail.com>
2024-07-20 22:46:45 +0700

    add request from aggregate to search api function

--- app/image_search/aggregate_api/utils.py
+++ app/image_search/aggregate_api/utils.py
@@ -1,6 +1,11 @@
 import json
+import os
 
 import numpy as np
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def load_image_path(save_path):
@@ -9,18 +14,28 @@ def load_image_path(save_path):
     return data
 
 
-def get_to_Embedding_vector(byte_image: bytes) -> np.ndarray:
+def get_embeddings(byte_image: bytes) -> np.ndarray:
     # Dummy implementation
     return np.random.rand(
-        128
+        len(byte_image), 512
     )  # Example vector size, replace with your actual implementation
 
 
+# def search(embedded_vector: np.ndarray) -> np.ndarray:
+#     # Dummy implementation
+#     return np.random.randint(
+#         0, 100, (len(embedded_vector), 5)
+#     )  # Example results, replace with your actual implementation
+
+
 def search(embedded_vector: np.ndarray) -> np.ndarray:
-    # Dummy implementation
-    return np.random.randint(
-        0, 100, 5
-    )  # Example results, replace with your actual implementation
+    r = requests.post(
+        f'{os.getenv("SEARCH_API_HOST")}:{os.getenv("SEARCH_API_PORT")}/search',
+        json={
+            "embeddings": embedded_vector.tolist(),
+        },
+    )
+    return r.json()
 
 
 async def search_images(files):
@@ -32,10 +47,7 @@ async def search_images(files):
     Returns:
         List[List[int]]: List of lists of indexes of similar images
     """
-    results = []
-    for file in files:
-        byte_image = await file.read()
-        embedded_vector: np.ndarray = get_to_Embedding_vector(byte_image)
-        result: np.ndarray = search(embedded_vector)
-        results.append(result.tolist())
+    # byte_image = await file.read()
+    embedded_vector = get_embeddings(files)
+    results = search(embedded_vector)
     return results
