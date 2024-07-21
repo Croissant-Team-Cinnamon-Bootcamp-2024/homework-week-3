00343008c573eacd03fb3ab82f72d4cb8cbf34be
huy.ng <nguyenduchuy1711@gmail.com>
2024-07-22 00:03:21 +0700

    reimplement embedding API and Dockerfile

--- app/image_search/embedding_api/utils/embedding.py
+++ app/image_search/embedding_api/utils/embedding.py
@@ -1,17 +1,23 @@
+from functools import lru_cache
 from typing import List
 
 import clip
-import numpy as np
 import torch
 
-from queryimage import QueryImage
+from .queryimage import QueryImage
+
+
+@lru_cache
+def load_clip_model(model_name, device="cpu"):
+    # Load the CLIP model
+    model, preprocess = clip.load(model_name, device=device)
+    return model, preprocess
 
 
 def get_image_embeddings(
     query_images: QueryImage, model_name="ViT-B/32", batch_size=32, device="cpu"
-) -> List[np.ndarray]:
-    # Load the CLIP model
-    model, preprocess = clip.load(model_name, device=device)
+) -> List[List[float]]:
+    model, preprocess = load_clip_model(model_name, device)
 
     # List to store image embeddings
     image_embeddings = []
@@ -25,6 +31,6 @@ def get_image_embeddings(
             ).to(device)
             embeddings = model.encode_image(preprocessed_images)
             embeddings /= embeddings.norm(dim=-1, keepdim=True)
-            image_embeddings.append(embeddings.cpu().numpy())
+            image_embeddings.extend(embeddings.cpu().numpy().tolist())
 
     return image_embeddings
